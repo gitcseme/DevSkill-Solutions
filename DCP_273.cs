38b4193e2e19ce3d63378b84a415509223822974
gitcseme <shuvo_CSE23@yahoo.com>
2019-01-24 13:09:41 +0600

    DCP 273

---
+++ DCP_273.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_Problems
+{
+    class DCP_273
+    {
+        static void Main (string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                string word = Console.ReadLine();
+                int len = word.Length;
+                bool easy = true;
+                for (int i = 0; i < len; ++i)
+                {
+                    if (word[i] == 'r')
+                    {
+                        easy = false;
+                        break;
+                    }
+                }
+                if (easy) {
+                    Console.WriteLine("Vely easy");
+                }
+                else {
+                    Console.WriteLine("Not vely easy");
+                }
+            }
+        }
+    }
+}
