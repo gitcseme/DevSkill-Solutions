b3ec14bc8533d9350fbbd6877a7a7822b63fff1a
gitcseme <shuvo_CSE23@yahoo.com>
2019-01-24 14:23:54 +0600

    DCP 184 and 4

---
+++ DCP_184.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_Problems
+{
+    class DCP_184
+    {
+        static void Main()
+        {
+            long[] fib = new long[50];
+            fib[1] = 1;
+            fib[2] = 3;
+
+            for (int i = 3; i <= 45; ++i)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(fib[n]);
+            }
+        }
+    }
+}
---
+++ DCP_4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_Problems
+{
+    class DCP_4
+    {
+        static void Main()
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] a = line.Split(' ');
+                int total = int.Parse(a[0]);
+                int n = int.Parse(a[1]);
+                int h = 0;
+                while (n-- > 0) {
+                    h += int.Parse(Console.ReadLine());
+                }
+
+                int ans = (int) Math.Ceiling((double)total / h);
+                if (ans > 1) {
+                    Console.WriteLine("Project will finish within {0} days.", ans);
+                }
+                else {
+                    Console.WriteLine("Project will finish within 1 day.");
+                }
+            }
+        }
+    }
+}
