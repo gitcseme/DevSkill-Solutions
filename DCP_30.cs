dabefb105e29edc96f66e2570009f22ad77e71e4
gitcseme <shuvo_CSE23@yahoo.com>
2019-01-26 00:03:34 +0600

    DCP_30

---
+++ DCP_30.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_Problems
+{
+    class DCP_30
+    {
+        static void Main ()
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int caseno = 1; caseno <= t; caseno++)
+            {
+                int[] cnt = new int[26];
+                for (int i = 0; i < 26; i++) cnt[i] = 0;
+                char[] c = Console.ReadLine().ToCharArray();
+
+                for (int i = 0; i < c.Length; i++) ++cnt[(c[i] - 'a')];
+
+                Console.WriteLine("Case {0}:", caseno);
+                for (int i = 0; i < 26; i++)
+                {
+                    if (cnt[i] != 0)
+                        Console.WriteLine("{0} {1}", (char)(97+i), cnt[i]);
+                }
+            }
+        }
+    }
+}
