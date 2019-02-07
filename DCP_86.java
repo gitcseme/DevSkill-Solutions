fa6034f4f1aaa43980682f2c039ef698d31f6045
gitcseme <shuvo_CSE23@yahoo.com>
2019-02-08 03:50:23 +0600

    DCP_86

---
+++ DCP_86.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
+class DCP_86 {
+
+	public static void main(String[] args) {
+	        Scanner in=new Scanner(System.in);
+	        int tc=in.nextInt();
+	        for(int p = 1; p <= tc; p++){
+	            BigInteger n=in.nextBigInteger();
+	            System.out.println("Case "+p+": "+n.multiply(n.subtract(BigInteger.ONE)).divide(new BigInteger("2")));
+	        }
+	}
+}
