2e21e7d2629be56ea0751b56d6404836ec5e9a87
gitcseme <shuvo_CSE23@yahoo.com>
2019-02-10 04:06:41 +0600

    DCP - 220, 254

---
+++ DCP_220.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_Problems
+{
+    class DCP_220
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int n;
+            for (int caseno = 1; caseno <= t; caseno++)
+            {
+                n = int.Parse(Console.ReadLine());
+                string[] s = Console.ReadLine().Split(' ');
+                Console.Write("Case {0}: ", caseno);
+
+                if (n == 1)
+                {
+                    Console.WriteLine(s[0]);
+                }
+                else
+                {
+                    Console.WriteLine("0");
+                }
+            }
+        }
+    }
+}
---
+++ DCP_254.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_Problems
+{
+    class DCP_254
+    {
+        static void Main(string[] args)
+        {
+            int p = int.Parse(Console.ReadLine());
+            int[] rate = new int[] { 1, 3, 10, -5 };
+            int max = int.MinValue;
+
+            for (int player = 1; player <= p; player++)
+            {
+                int score = 0;
+                string[] bols = Console.ReadLine().Split(' ');
+                for (int i = 0; i < 4; i++)
+                {
+                    score += (int.Parse(bols[i]) * rate[i]);
+                }
+
+                max = Math.Max(max, score);
+                if (score < 0) score = 0;
+
+                Console.WriteLine("Player {0}: {1}", player, score);
+            }
+            Console.WriteLine("High Score = {0}", max);
+        }
+    }
+}
