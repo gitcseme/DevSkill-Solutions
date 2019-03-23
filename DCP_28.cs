9352354f0cd452bfacd4118dea82e7a53d708ca3
kashuvo <shuvo_CSE23@yahoo.com>
2019-03-24 00:41:35 +0600

    DCP - 28

---
+++ DCP_28.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_problems
+{
+    class DCP_28
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            double R, S, sr;
+
+            while (t-- > 0)
+            {
+                string[] a = Console.ReadLine().Split(' ');
+                R = double.Parse(a[0]);
+                S = double.Parse(a[1]);
+
+                sr = Math.Sqrt(2 * S * S) / 2;
+                sr = Math.Round(sr, 2);
+
+                if (R == sr)
+                    Console.WriteLine("Yes");
+                else
+                    Console.WriteLine("No");
+            }
+
+        }
+    }
+}
