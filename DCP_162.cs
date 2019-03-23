abb5540093bdf4e6a0daff720a9d90675da49bbe
kashuvo <shuvo_CSE23@yahoo.com>
2019-03-24 02:23:27 +0600

    DCP - 162

---
+++ DCP_162.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_problems
+{
+    class DCP_162
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            long n, i;
+
+            while (t-- > 0)
+            {
+                n = long.Parse(Console.ReadLine());
+
+                for (i = (long)Math.Sqrt(n+0.01); i >= 1; --i)
+                    if (n % i == 0)
+                        break;
+
+                Console.WriteLine((n / i) - i);
+            }
+        }
+    }
+}
