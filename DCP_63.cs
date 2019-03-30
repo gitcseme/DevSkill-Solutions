4a0b27deba97822c54c21e1c1d13b19c0165909c
kashuvo <shuvo_CSE23@yahoo.com>
2019-03-30 15:46:20 +0600

    dcp - 63 string s+s contains all rotation of s

---
+++ DCP_63.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_problems
+{
+    class DCP_63
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            string a, b, s;
+
+            while (t-- > 0)
+            {
+                a = Console.ReadLine();
+                b = Console.ReadLine();
+                s = a + a;
+                if (a.Length != b.Length || !s.Contains(b))
+                    Console.WriteLine("NO");
+                else
+                    Console.WriteLine("YES");
+            }
+        }
+    }
+}
