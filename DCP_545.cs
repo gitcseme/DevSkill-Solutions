d3f78eb2f5de85bc1bcaf5c2d12c66b5002043cc
kashuvo <shuvo_CSE23@yahoo.com>
2019-04-03 23:47:53 +0600

    dcp - 545, 569 [Buffer size increased to = 1024]

---
+++ DCP_545.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_problems
+{
+    class DCP_545
+    {
+        static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(), Console.InputEncoding, false, bufferSize: 1024));
+
+            int K, t = int.Parse(Console.ReadLine());
+            int[] a = new int[110];
+
+            while (t-- > 0)
+            {
+                K = int.Parse(Console.ReadLine());
+
+                string[] arr = Console.ReadLine().Split();
+                for (int i = 0; i < 100; i++) a[i] = int.Parse(arr[i]);
+
+                int sum, mx = 100;
+                for (int i = 0; i < 100; i++)
+                {
+                    sum = 0;
+                    for (int j = i; j < 100; j++)
+                    {
+                        sum += a[j];
+                        if (sum >= K)
+                        {
+                            mx = Math.Min(mx, j - i + 1);
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine(mx);
+            }
+        }
+    }
+}
---
+++ DCP_569.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_problems
+{
+    class DCP_569
+    {
+        static void Main(string[] args)
+        {
+            int N, t = int.Parse(Console.ReadLine());
+            string name = "";
+            int skill, local;
+
+            while (t-- > 0)
+            {
+                N = int.Parse(Console.ReadLine());
+                skill = 0;
+                while (N-- > 0)
+                {
+                    string[] a = Console.ReadLine().Split(' ');
+                    local = int.Parse(a[1]);
+                    if (local >= skill)
+                    {
+                        name = a[0];
+                        skill = local;
+                    }
+                }
+
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
