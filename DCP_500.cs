d5da776a628ede2ef85821edbb20cdf0fdf75882
kashuvo <shuvo_CSE23@yahoo.com>
2019-04-19 01:06:55 +0600

    dcp - 500, 503

---
+++ DCP_500.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_problems
+{
+    class DCP_500
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            long a, b;
+
+            while (t-- > 0)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                a = long.Parse(s[0]);
+                b = long.Parse(s[1]);
+
+                if (b != 0 && a%b == 0)
+                    Console.WriteLine(":wink:");
+                else
+                    Console.WriteLine(":kick:");
+            }
+        }
+    }
+}
---
+++ DCP_503.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_problems
+{
+    class DCP_503
+    {
+        static void Main(string[] args)
+        {
+            //Console.SetIn(new StreamReader("../../inp.txt"));
+
+            int t = int.Parse(Console.ReadLine());
+            int n, j;
+
+            while (t-- > 0)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                n = int.Parse(s[0]);
+                j = int.Parse(s[1]);
+
+                List<int> KE = new List<int>();
+                List<int> DGM = new List<int>();
+                int id = 0;
+                string[] g = Console.ReadLine().Split(' ');
+                for (int i = 0; i < n; i++)
+                {
+                    ++id;
+                    int x = int.Parse(g[i]);
+                    if (x > j) KE.Add(id);
+                    else DGM.Add(id);
+                }
+
+                Console.WriteLine("Kache Eso");
+                if (KE.Count == 0) Console.WriteLine();
+                else
+                {
+                    StringBuilder ans = new StringBuilder();
+                    foreach (int e in KE) ans.Append(e + " ");
+                    Console.WriteLine(ans.ToString().TrimEnd());
+                }
+
+                Console.WriteLine("Dure Giya Mor");
+                if (DGM.Count == 0) Console.WriteLine();
+                else
+                {
+                    StringBuilder ans = new StringBuilder();
+                    foreach (int e in DGM) ans.Append(e + " ");
+                    Console.WriteLine(ans.ToString().TrimEnd());
+                }
+
+            }
+        }
+    }
+}
