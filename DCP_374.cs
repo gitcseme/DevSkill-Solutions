0cd184130049a20dff221b4362cb42ab1c5d55c4
kashuvo <shuvo_CSE23@yahoo.com>
2019-04-19 17:49:37 +0600

    dcp - 374

---
+++ DCP_374.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_problems
+{
+    class DCP_374
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            long n;
+
+            while (t-- > 0)
+            {
+                n = long.Parse(Console.ReadLine());
+                string bits = Convert.ToString(n, 2);
+                Console.WriteLine(bits.Length);
+            }
+        }
+    }
+}
